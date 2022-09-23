974bd80d85e74222e17b28fd30371f604611cb1d
jmersilus <Kingjoudel@icloud.com>
2022-09-22 22:04:02 -0500

    Working of Javascript

--- asset2/script.js
+++ asset2/script.js
@@ -1,8 +1,23 @@
-right.addEventListener("click", runCheck);
+document.getElementById("#wrong1")
+wrong1.addEventListener("click", wrongAnwser);
 
-function runCheck() {
+document.getElementById("#wrong2")
+wrong2.addEventListener("click", wrongAnwser);
 
-    window.alert("working")
+document.getElementById("#wrong3")
+wrong3.addEventListener("click", wrongAnwser);
+
+document.getElementById("#right")
+right.addEventListener("click", rightAnwser);
+
+function wrongAnwser(){
+    window.alert("wrong")
 }
 
-    
+function rightAnwser(){
+    window.alert("right")
+
+}
+
+// Figure out how to link right anwser to next set
+// Wrong anwser will give alert
